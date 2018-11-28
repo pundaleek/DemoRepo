58d8b593118c58c967b860578390677151b27279
pundalik <hpundalik33@gmail.com>
2018-11-28 18:53:13 +0530

    commit 1st

---
+++ additionofTwoNumbers.java
@@ -0,0 +1,9 @@
+class additionofTwoNumbers{
+	public static void main(String[] args){
+		int i = 10;
+		int j = 20;
+		int sum = i + j;
+		System.out.println("Sum of two numbers is: "+sum);
+		
+	}
+}
