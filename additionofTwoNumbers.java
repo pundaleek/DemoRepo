7130703c0453a82895ca35621f0ec54b0111b5e0
pundalik <hpundalik33@gmail.com>
2018-11-28 19:17:44 +0530

    Commit after adding variable k

--- additionofTwoNumbers.java
+++ additionofTwoNumbers.java
@@ -2,7 +2,8 @@ class additionofTwoNumbers{
 	public static void main(String[] args){
 		int i = 10;
 		int j = 20;
-		int sum = i + j;
+		int k = 120;
+		int sum = i + j + k;
 		Systeom.out.println(sum);
 		
 	}
